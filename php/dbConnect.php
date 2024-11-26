ee3845f485cb160420024885384507729ea4dc2d
KazukiMoriai <moriaikazuki@gmail.com>
2024-11-26 10:00:03 +0900

    さくらdb連携

--- php/dbConnect.php
+++ php/dbConnect.php
@@ -1,8 +1,12 @@
 <?php
 // DB接続
 try {
+    // ローカル
     $pdo = new PDO('mysql:dbname=kadai09_db;charset=utf8;host=localhost', 'root', '');
+
 } catch (PDOException $e) {
     exit('DBConnectError: ' . $e->getMessage());
 }
+
+
 ?>
